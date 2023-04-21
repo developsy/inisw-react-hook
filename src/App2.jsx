b4064153f450ea075330211116994ff15305b3b1
An Sol <apdlvmf486@gmail.com>
2023-04-21 09:46:48 +0900

    feat: 언마운트 기능 추가

--- src/App2.jsx
+++ src/App2.jsx
@@ -3,6 +3,8 @@ import Clock from "./Clock";
 
 const App2 = () => {
   const [isVisible, setIsVisible] = useState(false);
+  //현재는 마운트만 했지 언마운트를 하지 않아서 시계를 계속 토글하면 계속해서 마운트만 일어남. 네트워크라면 과부하가 걸린다.
+  //적절히 언마운트를 시키려면 useEffect를 수정한다.
   return (
     <div>
       <button onClick={() => setIsVisible(!isVisible)}> 시계를 토글합니다. </button>
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from "react";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
+  //적절히 언마운트 하려면 useEffect의 리턴값인 함수를 이용한다.
   useEffect(() => {
-    setInterval(() => {
+    console.log("### Clock component is mounted!!");
+    let handle = setInterval(() => {
       setTime(new Date());
+      console.log("### Time is updated");
     }, 1000);
+    return () => {
+      console.log("### Clock component will be unmounted!!");
+      clearInterval(handle);
+    };
   }, []);
+
   return (
     <div>
       <h2>{time.toTimeString()}</h2>
